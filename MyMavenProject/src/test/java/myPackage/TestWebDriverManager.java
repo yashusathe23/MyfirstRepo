cdc9872fdb41a08f45c153df8281de0caf41b0a3
DELL <DELL@DESKTOP-20MMUUN>
2026-04-17 17:08:29 +0530

    pushing the code

---
+++ MyMavenProject/src/test/java/myPackage/TestWebDriverManager.java
@@ -0,0 +1,23 @@
+package myPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestWebDriverManager {
+
+	@Test
+	public void a() {
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://facebook.com");
+
+	}
+
+}
