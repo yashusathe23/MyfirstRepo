87dc9f7258a1f4ddb536f959d5f47f44f19ed16b
DELL <DELL@DESKTOP-20MMUUN>
2026-04-21 12:27:26 +0530

    commented lines

--- MyMavenProject/src/test/java/myPackage/TestWebDriverManager.java
+++ MyMavenProject/src/test/java/myPackage/TestWebDriverManager.java
@@ -11,12 +11,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestWebDriverManager {
 
 	@Test
-	public void a() throws Exception {
-		WebDriverManager.edgedriver().setup();
-		WebDriver driver = new EdgeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://facebook.com");
-		driver.quit();
+	public void a() {
+		System.out.println("first change");
+		//WebDriverManager.edgedriver().setup();
+		//WebDriver driver = new EdgeDriver();
+		//driver.manage().window().maximize();
+		//driver.get("https://facebook.com");
+		//driver.quit();
 
 	}
 
